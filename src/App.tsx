ddbe262c1c56393ec9f16c3c0a2ec9af7fd5b12d
ProFreelancer <profreelancer258@gmail.com>
2024-06-20 16:54:40 +1200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-// src/App.tsx
-
 import React from 'react';
 import './App.css';
 import HomePage from './HomePage';
@@ -7,15 +5,7 @@ import HomePage from './HomePage';
 function App() {
   return (
     <div className="App">
-      <header>
-        {/* Add your header content here */}
-      </header>
-      <main>
-        <HomePage />
-      </main>
-      <footer>
-        {/* Add your footer content here */}
-      </footer>
+      <HomePage />
     </div>
   );
 }
